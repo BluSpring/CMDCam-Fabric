7dc2eaa4a95a301be1007698c8ae6bc4e816e9b6
Naz <bluspringindohq@gmail.com>
2023-11-17 14:33:31 +0800

    Initial port to Fabric

--- src/main/java/team/creative/cmdcam/common/command/builder/SceneCommandBuilder.java
+++ src/main/java/team/creative/cmdcam/common/command/builder/SceneCommandBuilder.java
@@ -4,7 +4,6 @@ import com.mojang.brigadier.arguments.BoolArgumentType;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.ArgumentBuilder;
-
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.commands.arguments.EntityArgument;
@@ -20,38 +19,38 @@ import team.creative.cmdcam.common.scene.CamScene;
 import team.creative.cmdcam.common.scene.attribute.CamAttribute;
 
 public class SceneCommandBuilder {
-    
+
     public static void scene(ArgumentBuilder<CommandSourceStack, ?> origin, CamCommandProcessor processor) {
         ArgumentBuilder<CommandSourceStack, ?> original = origin;
-        
+
         if (processor.requiresSceneName())
             origin = Commands.argument("name", StringArgumentType.string());
-        
+
         origin.then(Commands.literal("clear").executes((x) -> {
             processor.getScene(x).points.clear();
             processor.markDirty(x);
             x.getSource().sendSuccess(() -> Component.translatable("scene.clear"), false);
             return 0;
         }));
-        
+
         origin.then(new PointArgumentBuilder("add", (x, point) -> {
             processor.getScene(x).points.add(point);
             processor.markDirty(x);
             x.getSource().sendSuccess(() -> Component.translatable("scene.add", processor.getScene(x).points.size()), false);
         }, processor));
-        
+
         origin.then(new PointArgumentBuilder("insert", (x, point, index) -> {
             processor.getScene(x).points.add(index, point);
             processor.markDirty(x);
             x.getSource().sendSuccess(() -> Component.translatable("scene.insert", index + 1), false);
         }, processor));
-        
+
         origin.then(new PointArgumentBuilder("set", (x, point, index) -> {
             processor.getScene(x).points.set(index, point);
             processor.markDirty(x);
             x.getSource().sendSuccess(() -> Component.translatable("scene.set", index + 1), false);
         }, processor));
-        
+
         origin.then(Commands.literal("remove").then(Commands.argument("index", IntegerArgumentType.integer()).executes((x) -> {
             int index = IntegerArgumentType.getInteger(x, "index") - 1;
             CamScene scene = processor.getScene(x);
@@ -62,7 +61,7 @@ public class SceneCommandBuilder {
             processor.markDirty(x);
             return 0;
         })));
-        
+
         origin.then(Commands.literal("duration").then(Commands.argument("duration", DurationArgument.duration()).executes(x -> {
             long duration = DurationArgument.getDuration(x, "duration");
             if (duration > 0)
@@ -71,7 +70,7 @@ public class SceneCommandBuilder {
             x.getSource().sendSuccess(() -> Component.translatable("scene.duration", duration), false);
             return 0;
         })));
-        
+
         origin.then(Commands.literal("loops").then(Commands.argument("loop", IntegerArgumentType.integer(-1)).executes(x -> {
             int loop = IntegerArgumentType.getInteger(x, "loop");
             processor.getScene(x).loop = loop;
@@ -84,12 +83,12 @@ public class SceneCommandBuilder {
                 x.getSource().sendSuccess(() -> Component.translatable("scene.loops", loop), false);
             return 0;
         })));
-        
+
         ArgumentBuilder<CommandSourceStack, ?> tpO = Commands.literal("goto");
         ArgumentBuilder<CommandSourceStack, ?> tp = tpO;
         if (processor.requiresPlayer())
             tp = Commands.argument("players", EntityArgument.players());
-        
+
         tp.then(Commands.argument("index", IntegerArgumentType.integer(0)).executes(x -> {
             int index = IntegerArgumentType.getInteger(x, "index") - 1;
             CamScene scene = processor.getScene(x);
@@ -103,18 +102,18 @@ public class SceneCommandBuilder {
             origin.then(tpO.then(tp));
         else
             origin.then(tpO);
-        
+
         origin.then(Commands.literal("mode").then(Commands.argument("mode", CamModeArgument.mode()).executes(x -> {
             processor.getScene(x).setMode(StringArgumentType.getString(x, "mode"));
             return 0;
         })));
-        
+
         origin.then(new TargetArgumentBuilder("target", true, processor));
         origin.then(new TargetArgumentBuilder("follow", false, processor));
-        
+
         origin.then(new FollowArgumentBuilder(CamAttribute.PITCH, processor)).then(new FollowArgumentBuilder(CamAttribute.YAW, processor)).then(
             new FollowArgumentBuilder(CamAttribute.POSITION, processor));
-        
+
         origin.then(Commands.literal("interpolation").then(Commands.argument("interpolation", InterpolationArgument.interpolation()).executes((x) -> {
             String interpolation = StringArgumentType.getString(x, "interpolation");
             processor.getScene(x).interpolation = CamInterpolation.REGISTRY.get(interpolation);
@@ -122,7 +121,7 @@ public class SceneCommandBuilder {
             x.getSource().sendSuccess(() -> Component.translatable("scene.interpolation", interpolation), false);
             return 0;
         })));
-        
+
         origin.then(Commands.literal("smooth_start").then(Commands.argument("value", BoolArgumentType.bool()).executes((x) -> {
             boolean value = BoolArgumentType.getBool(x, "value");
             processor.getScene(x).smoothBeginning = value;
@@ -130,7 +129,7 @@ public class SceneCommandBuilder {
             x.getSource().sendSuccess(() -> Component.translatable("scene.smooth_beginning", value), false);
             return 0;
         })));
-        
+
         origin.then(Commands.literal("spinning_fix").then(Commands.argument("mode", CamPitchModeArgument.pitchMode()).executes((x) -> {
             CamPitchMode mode = CamPitchModeArgument.getMode(x, "mode");
             processor.getScene(x).pitchMode = mode;
@@ -138,7 +137,7 @@ public class SceneCommandBuilder {
             x.getSource().sendSuccess(() -> Component.translatable("scene.pitch_mode", mode), false);
             return 0;
         })));
-        
+
         origin.then(Commands.literal("distance_timing").then(Commands.argument("value", BoolArgumentType.bool()).executes((x) -> {
             boolean value = BoolArgumentType.getBool(x, "value");
             processor.getScene(x).distanceBasedTiming = value;
@@ -146,10 +145,9 @@ public class SceneCommandBuilder {
             x.getSource().sendSuccess(() -> Component.translatable("scene.distance_timing", value), false);
             return 0;
         })));
-        
+
         if (processor.requiresSceneName())
             original.then(origin);
-        
+
     }
-    
 }
