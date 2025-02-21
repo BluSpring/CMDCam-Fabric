ec5850aecc0bbb5c3d9fad39648bbf72e7490f45
singlerr <singlerr@naver.com>
2025-02-21 22:41:01 +0900

    removed porting lib from dependency

---
+++ src/main/java/io/github/fabricators_of_create/porting_lib/features/MinecraftClientUtil.java
@@ -0,0 +1,23 @@
+package io.github.fabricators_of_create.porting_lib.features;
+
+import io.github.fabricators_of_create.porting_lib.mixins.client.MinecraftAccessor;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.language.LanguageManager;
+
+import java.util.Locale;
+
+@Environment(EnvType.CLIENT)
+public final class MinecraftClientUtil {
+	public static float getRenderPartialTicksPaused(Minecraft minecraft) {
+		return get(minecraft).port_lib$pausePartialTick();
+	}
+
+
+	private static MinecraftAccessor get(Minecraft minecraft) {
+		return (MinecraftAccessor) minecraft;
+	}
+
+	private MinecraftClientUtil() {}
+}
